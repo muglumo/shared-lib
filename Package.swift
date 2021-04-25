437d2bbf4f2c3c2db8636f727240ae35340f0ad6
Mohamed Tolba Sayed <>
2021-04-25 23:30:15 +0200

    Add KMM framework and realm

--- Package.swift
+++ Package.swift
@@ -15,16 +15,24 @@ let package = Package(
             targets: ["shared-lib"]),
     ],
     dependencies: [
+        .package(url: "https://github.com/realm/realm-cocoa.git", .upToNextMajor(from: "10.7.3")),
         .package(path: "/KMM/shared.framework")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        
         .target(
             name: "shared-lib",
-            dependencies: []),
+            dependencies: []
+        ),
+        .binaryTarget(
+            name: "shared",
+            path: "/KMM/shared.framework"
+        ),
         .testTarget(
             name: "shared-libTests",
-            dependencies: ["shared-lib"]),
+            dependencies: ["shared-lib"]
+        ),
     ]
 )

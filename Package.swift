062e2f4e78642a2a970a70744d356d70cfdf93b5
Mohamed Tolba Sayed <>
2021-04-30 19:59:10 +0200

    add target

--- Package.swift
+++ Package.swift
@@ -12,10 +12,10 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "shared-lib",
-            targets: ["shared-lib"]),
+            targets: ["shared-lib", "shared"]),
     ],
     dependencies: [
-        .package(path: "KMM/shared.xcframework")
+        // dependencies
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
